d3928fbac8044dffd4821003ba62c86ebfcb1689
Mikhail Shirkov <shirkevich@gmail.com>
2010-02-22 00:14:54 +0300

    move sergs work to github

---
+++ about.markdown
@@ -0,0 +1,6 @@
+### Abshands by [clique][1]
+
+Abshands is theme created for [abshands.ru][2]
+
+[1]: http://clique.ru/
+[2]: http://abshands.ru/
---
+++ helpers/theme_helper.rb
@@ -0,0 +1,12 @@
+ContentHelper.module_eval do
+  def category_links(article)
+    article.categories.map { |c| link_to h(c.name), category_url(c), :rel => 'tag'}.join(" ")
+  end
+
+  def tag_links(article)
+    article.tags.map { |tag| link_to tag.display_name, tag.permalink_url, :rel => "tag"}.sort.join(" ")
+  end
+end
+
+SearchSidebar.view_root = File.dirname(__FILE__) + '/../views/plugins/search_sidebar'
+TagSidebar.view_root = File.dirname(__FILE__) + '/../views/plugins/tag_sidebar'
